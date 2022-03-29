bafd90538787c6cc964d4c5627141d2be55a5676
ayanas-git <29583621+ayanas-git@users.noreply.github.com>
2022-03-29 18:58:07 -0400

    Includes updated model selection

--- Forecast_Avocado_Prices.R
+++ Forecast_Avocado_Prices.R
@@ -113,6 +113,17 @@ gridExtra::grid.arrange(a, b, c, ncol = 1,  top = "Representation of Data Betwee
 # The US also gets a summer supplement from Peru, a winter bump from Chile
 # The season runs July through April in Chile and May through August in Peru and Argentina. Maybe a Peru import ~May
 
+# Time Series Decomposition----
+# Decomposition methods allow us to break time series into their components trend + season + remainder
+dcmp <- av_ts %>%
+  model(STL(AveragePrice))
+components(dcmp)
+
+# Plot all the components
+components(dcmp) %>% 
+  autoplot() + 
+  xlab("Date")
+# Monthly?, Definite 3rd quarter, and June/July 2015 (for Organic only?)
 
 
 # Training data and Cross-Validation----
@@ -123,7 +134,7 @@ gridExtra::grid.arrange(a, b, c, ncol = 1,  top = "Representation of Data Betwee
 # Just to see the years out and get a visual line we can look at but understanding
 # the forecast will be for a short period, i.e. 2-3 weeks
 
-av_fit <- av_train %>%
+av_fits_try <- av_train %>%
   model( SeasNaive = SNAIVE(AveragePrice ~ lag("year")),
          STL = decomposition_model(STL(AveragePrice), ETS(season_adjust)), 
          STL.log = decomposition_model(STL(log(AveragePrice)), ETS(season_adjust)), 
@@ -134,10 +145,10 @@ av_fit <- av_train %>%
   labs(title = "Average US Avocado Prices", y="$US") +
   guides(colour=guide_legend(title="Forecast")) 
 
-av_fit
-
-# Not understanding ETS, the data is seasonal. Seasonal selections, no fc output. 
+av_fits_try
 # (Leaving this for funny code commentary. Apparently I am yelling at R that there is seasonality. )
+# Not understanding ETS, the data is seasonal. Seasonal selections, no fc output. 
+
 
 # NTS: 
 # What is happening?
@@ -147,66 +158,85 @@ av_fit
 # Continue to suspect a pattern, if not seasonal then possibly cyclical.
 # Of the models, two had acceptable “start” points for the forecasted region, the ETS and ARIMA. 
 
+# Time Series Decomposition
+# Decomposition methods allow us to break time series into their components trend + season + remainder
+dcmp <- av_ts %>%
+  model(STL(AveragePrice))
+components(dcmp)
+# February not surprising from ggseason() visual
 
-# MODEL Selection + Bonus Work Continued
+# Plot all the components
+components(dcmp) %>% 
+  autoplot() + 
+  xlab("Date")
+# Monthly?, Definite 3rd quarter, and June/July 2015 (for Organic only?)
+
+# Compare models
+
+# Looking at what we have to chose from, whats is best? COmparing using an auto ETS here.
+av_mods_all <- av_train %>%
+  model(ARIMA = ARIMA(AveragePrice, stepwise = FALSE), 
+        ETS = ETS(AveragePrice), 
+        ETSaan = ETS(AveragePrice ~ error("A") + trend ("A") + season("N")),             
+        SeasNaive = SNAIVE(AveragePrice ~ lag("year")),
+        STL = decomposition_model(STL(AveragePrice), ETS(season_adjust)), 
+        STL.log = decomposition_model(STL(log(AveragePrice)), ETS(season_adjust))) 
+accuracy(av_mods_all)
+# Best MAPE = STL decomposition, but clearly something is off!
+
+# Cross-validation - toss SeasNaive (clearly the worst) and the log-transformed (simplicity)
+# and it's values are very close to the non-transformed STL
+av_cv <- av_train %>%
+  stretch_tsibble(.init = 25, .step = 1) # Short step ahead
+
+av_mods <- av_train %>% 
+  model( ARIMA = ARIMA(AveragePrice, stepwise = FALSE), 
+         ETS = ETS(AveragePrice),
+         ETSaan = ETS(AveragePrice ~ error("A") + trend ("A") + season("N")),
+         STL = decomposition_model(STL(AveragePrice), ETS(season_adjust)),) 
+
+av_mods %>%
+  forecast(h = 2) %>%
+  accuracy(av_ts)  
+# ETS is the best method based upon MAPE, MAE, and RMSE.
+# Also, ETS spit out as (A,N,N) - but that does not work. Forced an (A, A, N) for 
+# the ETS model and it fit better than the ARIMA (remember for short forecast only) and the default ETS model.
+# I could see for just that short period of time, an increase is plausible. Keeping.
+
+# MODEL Selection----
 ##################################################################
 
-# Moving forward, keeping ETS and ARIMA for acceptable comparisons 
-# Even though the other models had some shape (forcing them to account for seasonality) 
-# it is too far off. The final forecaset period, may be very short. 
+# Moving forward, keeping ETS(AveragePrice)
 
-# Model Trys
+# Model Information
 av_fit_ARIMA <- av_train %>% 
-  model(searchARIMA = ARIMA(AveragePrice, stepwise = FALSE))
+  model(ARIMA = ARIMA(AveragePrice, stepwise = FALSE))
 av_fit_ARIMA
 report(av_fit_ARIMA)
 # Get an ARIMA(0,1,0) - Random Walk
 
 av_fit_ETS <- av_train %>% 
-  model(ETS = ETS(AveragePrice))
+  model(ETS = ETS(AveragePrice ))
 av_fit_ETS
 report(av_fit_ETS)
+
+av_fit_ETSaan <- av_train %>% 
+  model(ETSaan = ETS(AveragePrice ~ error("A") + trend ("A") + season("N")))
+av_fit_ETSaan
+report(av_fit_ETSaan)
 # Get an ETS(A,N,N) - Simple exponential smoothing with additive errors
 # alpha is almost 1, just about completely leveled
 
 
-av_fit_ETSaan <- av_train %>% 
-  model(ETSaan = ETS(AveragePrice ~ error("A") + trend ("A") + season("N")))
-av_fit_ETSaa
-report(av_fit_ETSann)
-# ETS(A,A,N) Holt’s method with additive errors is plotting
-# While unsuitable for long-term forecasting looks ok for a very short forecast 
+# While unsuitable for long-term forecasting looks consider for a very short forecast 
 # (<= 2-3 weeks, absolutely no more than 4) In my limited opinion
 
-# Compare models
-#we can smooth out the data to make it less noisy using a Moving Average (MA)
-#we do that using the slide_dbl function
-av_fit <- av_train %>%
-  model( SeasNaive = SNAIVE(AveragePrice ~ lag("year")),
-         STL = decomposition_model(STL(AveragePrice), ETS(season_adjust)), 
-         STL.log = decomposition_model(STL(log(AveragePrice)), ETS(season_adjust)), 
-         ETS = ETS(AveragePrice ~ error("A") + trend ("A") + season("N")), 
-         searchARIMA = ARIMA(AveragePrice, stepwise = FALSE))  %>%
-  forecast(h=104) %>%
-  autoplot(av_ts, level = NULL) + 
-  labs(title = "Average US Avocado Prices", y="$US") +
-  guides(colour=guide_legend(title="Forecast")) 
 
-av_fit
-
-# Looking at what we have to chose from, whats is best?
-av_fits <- av_train %>%
-  model( SeasNaive = SNAIVE(AveragePrice ~ lag("year")),
-         STL = decomposition_model(STL(AveragePrice), ETS(season_adjust)), 
-         STL.log = decomposition_model(STL(log(AveragePrice)), ETS(season_adjust)), 
-         ETS = ETS(AveragePrice ~ error("A") + trend ("A") + season("N")), 
-         searchARIMA = ARIMA(AveragePrice, stepwise = FALSE)) 
-accuracy(av_fits)
 
 # Ok, so is our data Stationary?
 # A stationary time series is one whose STATISTICAL properties do not depend on the time at 
 # which the series is observed. Thus, time series with trends, or with seasonality, 
-# are not stationary.... (Forecasting: P rinciples and Practice)
+# are not stationary.... (Forecasting: Principles and Practice)
 
 # FURTHERMORE...
 # In general, a stationary time series will have no predictable patterns in the long-term. 
@@ -218,13 +248,10 @@ av_ts %>%
   features(diff_price, ljung_box, lag=2)
 
 av_ts %>%
-  model(ETS(Avg.Price)) %>%
-  gg_tsresiduals()
-
-av_ts %>%
-  model(ARIMA(AveragePrice)) %>%
+  model(ETS(AveragePrice)) %>%
   gg_tsresiduals() + 
   labs(title = "Changes in Average US Avocado Prices")
+
 # Potential issue (~every 10Wks), cyclical - not seasonal?!?
 # The tides turn every 9/10 weeks?
 # One autocorrelation is outside of the 95% limits, Ljung statistic (.864 for h =2)
@@ -240,166 +267,24 @@ av_ts %>%
 av_ts%>%
   features(difference(AveragePrice), unitroot_kpss)
 
-# it does.  We will use a value of d=1 for our models.
+# it does.  We would use a value of d=1 for our models in p,d,q.
 av_ts %>% 
   ACF(AveragePrice, lag_max = 35) %>% 
   autoplot() + 
   labs(title = "Average US Avocado Prices - Depreciating ACF")
 # Depreciating
 
-# let's view the ACF and PACF of the DIFFERENCED data
+# View the ACF and PACF of the DIFFERENCED data
 av_ts %>%
   gg_tsdisplay(difference(AveragePrice), plot_type = 'partial') + 
   labs(title = "US Avocado Average Weekly Prices")
-
 # both ACF and PACF could be described as sinusoidal.
 
-#let's also add in some automatic models 
-av_arima_fits <- av_ts %>%
-  model(arima010 = ARIMA(AveragePrice ~ pdq(0,1,0)),
-        stepwise = ARIMA(AveragePrice),
-        bestARIMA = ARIMA(AveragePrice, stepwise = FALSE)
-  )
-# First two are exactly the same, stepwise = TRUE (default)
-av_arima_fits %>% pivot_longer(names_to = "Model name", values_to = "AveragePrice")
-
-accuracy(av_arima_fits)
-glance(av_arima_fits)
-
-# Use the "bestARIMA" model for a short forecast.
+# Use the ETS model for a short forecast.
 av_ARIMA_fc <- av_ts %>%
-  model(ARIMA = ARIMA(AveragePrice, stepwise = FALSE))  %>%
+  model(ETSaan = ETS(AveragePrice ~ error("A") + trend ("A") + season("N")))  %>%
   forecast(h=4) %>%
-  autoplot(av_ts, level = NULL) + 
-  labs(title = "Average US Avocado Prices - Short Forecast ", y="$US") +
-  guides(colour=guide_legend(title="Forecast")) 
+  autoplot(av_ts, level = c(80,95)) + 
+  labs(title = "Average US Avocado Prices - Short Forecast ", y="$US")
 av_ARIMA_fc
 
-#######
-# TIME PERMITTING
-
-# One more try at a model. ARIMA was good, with potential cyclical activity trying Gaussian method a 
-# type of autoregression (periodic)?
-
-library(GauPro)
-
-
-# (X, y)
-av_gp <- GauPro(av_train$Date, av_train$Avg.Price, parallel = FALSE)
-# Testing if worked, prediction for 1.15
-predict(av_gp,1.15) # The predictor works
-
-plot(av_train$Date, av_train$Avg.Price)
-curve(av_gp$predict(av_train$Date), add = TRUE, col=2)
-
-
-#####
-library(GPfit)
-
-av_gp <- GP_fit(av_train$Date, av_train$Avg.Price)
-
-plot(av_gp)
-
-
-
-
-
-# Monthly data try
-
-# Tidy
-# Create tsibble of avocado data 
-# Keep = TotalUS Conventional Avocado AveragePrices over time)
-
-# datStart = min(av_ts$Date) 2015-01-01
-
-library(wktmo)
-av$MonthYr <- strftime((av$Date), format = "%Y-%m")
-table(av$MonthYr)
-
-av_ts <- av %>% 
-  filter(region == 'TotalUS', type == 'conventional') %>%
-  mutate(Date = as.Date(Date, format = "%Y-%m-%d")) %>%
-  arrange(Date) %>%
-  select(Date, AveragePrice) %>% 
-  rename(Avg.Price = AveragePrice) %>%
-  as_tsibble(index = Date)
-
-av_ts <- av %>% 
-  filter(region == 'TotalUS', type == 'conventional') %>%
-  mutate(Date = as.Date(Date, format = "%Y-%m-%d"), 
-         MonthYr = yearmonth(MonthYr, format = "%Y-%m"),
-         AvgPrice = mean(AveragePrice)) %>%
-  arrange(MonthYr) %>%
-  select(MonthYr, AveragePrice) %>% 
-  rename(Avg.Price = AveragePrice) %>%
-  as_tsibble(index = MonthYr)
-
-
-av_ts %>%
-  autoplot(Avg.Price) +
-  labs(title = "Average US Avocado Prices Over Time", 
-       y = "Average Price ($)", 
-       x = "Date")
-# Seasonality (Y), Multiplicative over time?
-# IT IS CYCLICAL... NOT SEASONAL! .... Hmmmmmmmm.
-
-
-##########################################################################
-av_modWork <- av_train %>%
-  model( SeasNaive = SNAIVE(Avg.Price ~ lag("year")),
-         STL = decomposition_model(STL(Avg.Price), ETS(season_adjust)), 
-         STL.log = decomposition_model(STL(log(Avg.Price)), ETS(season_adjust)), 
-         ETS = ETS(Avg.Price ~ error("A") + trend ("A") + season("N")))
-av_modWork
-#view fitted values and residuals
-augment(av_modWork)
-
-#forecast
-av_fc_all <- av_modWork %>%
-  forecast(h = 104) #Doing it 104 time periods out, 104 time periods = 2 years
-av_fc_all 
-
-##########################################################################
-
-# Reshape data to monthly, give it a go (ETS, ARIMA)
-
-
-
-
-
-####################### TAKE A LOOK AT REG ###################
-
-
-#Plot on the y consumption and income overlapping each other
-av_ty <- av %>% 
-  filter(region == 'TotalUS', type == 'conventional') %>%
-  mutate(Date = as.Date(Date, format = "%Y-%m-%d")) %>%
-  arrange(Date) %>%
-  select(Date, AveragePrice, Total.Volume) %>% 
-  rename(Avg.Price = AveragePrice) %>%
-  as_tsibble(index = Date)
-av_ty
-
-av_ty %>%
-  ggplot(aes(x = Date)) +
-  geom_line(aes(y = Avg.Price, colour = "Price")) +
-  geom_line(aes(y = log(Total.Volume), colour = "Vol")) +
-  labs(x = "Year", y ="% change") +
-  guides(colour=guide_legend(title="series"))
-
-
-# You can see the relationship in a scatter plot (this is the preferred plot to view before a regression)
-av_ty %>%
-  ggplot(aes(x = Total.Volume, y = Avg.Price)) +
-  labs(x = "Vol", y ="Price") +
-  geom_point() +
-  geom_smooth(method="lm", se=FALSE) #this line provides us with a simple linear regression of Consumption vs Income
-
-
-# The tslm function fits a regression model with time series data
-fit_consMR1 <- av_ty %>%
-  model(TSLM(Total.Volume ~ Avg.Price)) 
-
-report(fit_consMR1)
-
-model()
